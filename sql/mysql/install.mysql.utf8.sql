29a8b1c7e34e8037e62dcf03fe5f69433f08dae2
Trijal-Bhardwaj <Trijalbhardwaj00@gmail.com>
2022-04-19 19:37:18 +0530

    Added formplugin.xml File Code Linked With PHP Files & SQL

--- sql/mysql/install.mysql.utf8.sql
+++ sql/mysql/install.mysql.utf8.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `#__customform` (
   `articleID` int(10) NOT NULL AUTO_INCREMENT, 
-  `formField1` varchar(30) NOT NULL, 
-  `formField2` varchar(30) NOT NULL, 
+  `formPlugin1` varchar(30) NOT NULL, 
+  `formPlugin2` varchar(30) NOT NULL, 
   PRIMARY KEY (`articleID`)
 ) ENGINE = INNODB DEFAULT CHARSET = utf8 AUTO_INCREMENT = 1;
